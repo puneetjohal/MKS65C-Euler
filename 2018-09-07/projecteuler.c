0e6cb6390899407af39c82fead3f2f699a1fd831
Puneet <pjohal@stuy.edu>
2018-09-12 06:07:56 -0400

    proj euler

--- 2018-09-07/projecteuler.c
+++ 2018-09-07/projecteuler.c
@@ -2,13 +2,14 @@
 
 int main(){
   //problem 1
-  int i,sum;
+  int i;
+  int sum = 0;
   for (i=1 ; i<=1000 ; i+=1){
     if (i%3==0 || i%5==0){
       sum += i;
     }
   }
-  printf("Sum of all multple of 3 or 5 between 0 and 1000: %d \n",sum);
+  printf("Sum of all multiples of 3 or 5 between 0 and 1000: %d \n", sum);
   //problem 5
   int divis = 0;
   int num = 0;
